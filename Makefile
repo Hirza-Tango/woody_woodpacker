2ef776d7f97e05981acef150e6f1c94e8568cc4c
Dylan SLOGROVE <dslogrov@c4r7s4.wethinkcode.co.za>
2019-10-07 08:16:25 +0200

    Init

---
+++ Makefile
@@ -0,0 +1,38 @@
+NAME=woody_woodpacker
+DEPS=
+LIBFT_DIR=.
+INCLUDES=$(LIBFT_DIR)/includes
+REL_DEPS= $(DEPS:%=$(LIBFT_DIR)/%)
+CC=gcc
+CFLAGS=-Wall -Wextra -Werror -I . -I $(INCLUDES) -Ofast -std=c18
+ifdef COMPILER_ARGS
+CFLAGS+= -D$(COMPILER_ARGS)
+endif
+CFILES=
+
+OBJ=$(CFILES:%.c=build/%.o)
+
+$(NAME): $(OBJ) $(REL_DEPS)
+	@gcc $(CFLAGS) -o $(NAME) $^ -ltermcap
+
+$(REL_DEPS):
+	@make -C $(dir $@)
+
+build/%.o: %.c
+	@mkdir -p build
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+all: $(NAME);
+
+clean fclean re::
+	@for file in $(dir $(REL_DEPS)) ; do $(MAKE) -C $$file $@ ; done
+
+clean::
+	@rm -rf build/
+
+fclean:: clean
+	@rm -rf $(NAME)
+
+re:: fclean all
+
+.PHONY: clean fclean re all $(REL_DEPS)
---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 07:00:07 by dslogrov          #+#    #+#             */
+/*   Updated: 2019/10/07 08:09:22 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <woody.h>
+
+int main(int argc, char *argv[]){
+	//handle arguments
+	//try open file
+		//catch perror
+	//map file to memory
+	//find text section and segment
+	//try insert decryptor at end
+	//encrypt text section
+	//save entry point, start address and size of text section
+	//change entry point to decryptor
+
+}
